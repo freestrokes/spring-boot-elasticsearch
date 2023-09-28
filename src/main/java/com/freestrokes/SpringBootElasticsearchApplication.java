d14897e34fba4dd44aea3d4fad551f5d34364a60
freestrokes <freestrokes.hk@gmail.com>
2023-09-28 18:00:30 +0900

    [main] feat: 프로젝트명 수정

--- src/main/java/com/freestrokes/SpringBootElasticsearchApplication.java
+++ src/main/java/com/freestrokes/SpringBootElasticsearchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootJpaApplication {
+public class SpringBootElasticsearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJpaApplication.class, args);
+		SpringApplication.run(SpringBootElasticsearchApplication.class, args);
 	}
 
 }
--- src/test/java/com/freestrokes/SpringBootElasticsearchApplicationTests.java
+++ src/test/java/com/freestrokes/SpringBootElasticsearchApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-public class SpringBootJpaApplicationTests {
+public class SpringBootElasticsearchApplicationTests {
 
 	@Test
 	public void contextLoads() {
